e80917521cf03459ce360e0c0687ca3abb91c797
Eric Robertson <aerrober@amazon.com>
2021-06-29 15:24:32 -0700

    Initial Commit

---
+++ debug_snake/README.md
@@ -0,0 +1,5 @@
+# DEBUG SNAKE
+
+Snake for battlesnakes that is ment to debug my local implementation
+
+Simply exists
---
+++ debug_snake/Snake.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Actual logic for the Battlesnake
+
+def start ( request_data ):
+    print( request_data )
+    return "left"
+
+def move ( request_data):
+    return "left"
+
+
+# Utils
+
+def generate_board_constuct ( game_state ):
+    pass
