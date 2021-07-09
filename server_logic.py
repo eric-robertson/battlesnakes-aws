c093a086c27e9047742fdf616ff690efd619e70c
EC2 Default User <ec2-user@ip-172-26-6-177.us-west-2.compute.internal>
2021-07-09 18:20:17 +0000

    bug fixes

--- server_logic.py
+++ server_logic.py
@@ -6,31 +6,31 @@ from util import *
 
 def avoid_neck(my_head: Dict[str, int], my_body: List[dict], possible_moves: List[str]) -> List[str]:
     my_neck = cvt_pt(my_body[1])  # The segment of body right after the head is the 'neck'
-    if my_neck[0] < my_head[0]:  # my neck is left of my head
+    if my_neck.x < my_head.x:  # my neck is left of my head
         possible_moves['left'] = -1
-    elif my_neck[0] > my_head[0]:  # my neck is right of my head
+    elif my_neck.x > my_head.x:  # my neck is right of my head
         possible_moves['right'] = -1
-    elif my_neck[1] < my_head[1]:  # my neck is below my head
+    elif my_neck.y < my_head.y:  # my neck is below my head
         possible_moves['down'] = -1
-    elif my_neck[1] > my_head[1]:  # my neck is above my head
+    elif my_neck.y > my_head.y:  # my neck is above my head
         possible_moves['up'] = -1
 
     return possible_moves
 
 def avoid_walls(height: int, width: int, head, possible_moves):
-    if head[0] == width - 1:
+    if head.x >= width:
         possible_moves['right'] = -1
-    if head[0] == 0:
+    if head.x <= 1:
         possible_moves['left'] = -1
-    if head[1] == height - 1:
+    if head.y >= height:
         possible_moves['up'] = -1
-    if head[1] == 0:
+    if head.y <= 1:
         possible_moves['down'] = -1
     return possible_moves
 
 def avoid_snakes(possible_moves: dict, board) -> dict:
     for move, pos in possible_moves.items():
-        if pos != -1 and board[pos[0]][pos[1]] > 1:
+        if pos != -1 and board[pos.x][pos.y] > 1:
             possible_moves[move] = -1
     return possible_moves
 
@@ -71,25 +71,28 @@ def choose_move(data: dict) -> str:
     possible_moves = avoid_walls(board_height, board_width, my_head, possible_moves)
     
     # matrix representation of the board
-    board = np.zeros((board_height, board_width)) # zero indicates open space
+    board = np.zeros((board_height + 2, board_width + 2)) # zero indicates open space
     i = len(my_body)
     for d in my_body:
-        board[d['x']][d['y']] = i
+        point = cvt_pt(d)
+        board[point.x][point.y] = i
         i -= 1
     for snake in data['board']['snakes']:
         i = len(snake['body'])
         for d in snake['body']:
-            board[d['x']][d['y']] = i
+            point = cvt_pt(d)
+            board[point.x][point.y] = i
             i -= 1
     
-    for f in data['food']:
-        board[f['x']][f['y']] = -1 # -1 indicated food
+    for f in data['board']['food']:
+        point = cvt_pt(f)
+        board[point.x][point.y] = -1 # -1 indicated food
    
     # avoid other snakes yes
     possible_moves = avoid_snakes(possible_moves, board)
     # try to avoid head-on collisions if possible
     possible_moves = avoid_head_on(possible_moves, data, my_head)
-    # TODO: try to avoid closing itself off - minimax search
+    # choose random move from remaining options if there are any
     rem = remaining_moves(possible_moves)
     move = "left"
     if len(rem) > 0:
--- util.py
+++ util.py
@@ -9,7 +9,7 @@ MOVES = ['up', 'down', 'left', 'right']
 
 # manhattan distance
 def mh_dist(p1, p2):
-    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+    return abs(p1.x - p2.x) + abs(p1.y - p2.y)
 
 def remaining_moves(possible_moves):
     rem = []
